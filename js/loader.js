104208d97af6c71382e62ebcfab69111c7058dbd
sayyedbasit21 <sayyedbasit21@gmail.com>
2020-04-19 12:25:18 +0530

    Published my second work
    Simple HTML Templates using css jquery and javascript

---
+++ js/loader.js
@@ -0,0 +1,14 @@
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.myreview').carousel({
+      numVisible:5,
+      shift:70,
+      padding:60
+    });
+    $('.slider').slider({
+      fullWidth:true
+    });
+    $('.parallax').parallax();
+  });
+ 
